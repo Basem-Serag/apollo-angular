02f763fd90e064c82c768a25553204334d32f6a9
basem <basem@basem.com>
2024-06-11 11:10:52 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'apollo-angular';
+  rates!: any[];
+  loading = true;
+  error: any;
+  constructor(private readonly apollo: Apollo) {}
+
+  ngOnInit(): void {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.rates = result.data?.rates;
+        this.loading = result.loading;
+        this.error = result.error;
+      });
+  }
 }
